530cd473f2efef92b3cf291e3f46141da984b1cc
Snobik57 <snobik57@gmail.com>
2022-12-16 17:09:13 +0600

    ADD: binary search

--- main.py
+++ main.py
@@ -2,3 +2,23 @@
 
 def some_func(a, b):
     return a * b
+
+
+def binary_search(iteration: list, item):
+    """
+    Метод позовляет найти индекс item в списке iteration с помощью бинарного (Логорифмического) поиска
+    """
+    low = 0
+    high = len(iteration) - 1
+
+    while low <= high:
+        mid = int((low + high) / 2)
+        guess = iteration[mid]
+        if guess == item:
+            return f"index = {mid}"
+        elif guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return None
