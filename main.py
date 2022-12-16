8f987af86a4053eef5412d4104ec9004dd7f3581
Snobik57 <snobik57@gmail.com>
2022-12-16 17:15:14 +0600

    ADD: logarithm

--- main.py
+++ main.py
@@ -2,3 +2,17 @@
 
 def some_func(a, b):
     return a * b
+
+
+def log(numeral, base=2):
+    """
+    Метод преднозначен для подсчета логорифма от: numeral, с осонованием: base
+    numeral: число
+    base: основание (по-умолчанию = 2)
+    """
+    y = 0
+    while numeral > 1:
+        y += 1
+        numeral = numeral / base
+
+    return y
